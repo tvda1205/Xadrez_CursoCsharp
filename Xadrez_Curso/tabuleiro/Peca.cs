65864bb0fd4dfffefee9fbe02c7476f757ef6b3f
tvda1205 <tvda1205@gmail.com>
2024-11-07 15:51:40 -0300

    Começando a implementar o tabuleiro

---
+++ Xadrez_Curso/tabuleiro/Peca.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xadrez_Curso.tabuleiro
+{
+    class Peca
+    {
+        public Posicao Posicao { get; set; }
+        public Cor Cor { get; protected set; }
+        public int QtdeMovimentos { get; protected set; }
+
+        public Tabuleiro tabuleiro { get; protected set; }
+
+        public Peca(Posicao posicao, Cor cor, Tabuleiro tabuleiro)
+        {
+            Posicao = posicao;
+            Cor = cor;
+            this.tabuleiro = tabuleiro;
+            QtdeMovimentos = 0;
+        }
+    }
+}
